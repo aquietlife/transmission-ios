a51c140ffa2e782754dbd1e167aec6f3fa5c5f83
Johann Diedrick <jdiedrick@gmail.com>
2014-08-26 16:19:09 -0400

    Initial Commit

---
+++ transmission-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  transmission-ios
+//
+//  Created by Johann Diedrick on 8/26/14.
+//  Copyright (c) 2014 Johann Diedrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ transmission-iosTests/transmission_iosTests.m
@@ -0,0 +1,34 @@
+//
+//  transmission_iosTests.m
+//  transmission-iosTests
+//
+//  Created by Johann Diedrick on 8/26/14.
+//  Copyright (c) 2014 Johann Diedrick. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface transmission_iosTests : XCTestCase
+
+@end
+
+@implementation transmission_iosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
